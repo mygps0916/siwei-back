da737fa63706ecc521ca099b54b5a9c32e9b1c88
chenzr0916 <mygps0916@163.com>
2021-10-29 11:33:11 +0800

    首次提交工程

---
+++ siwei-auth/src/main/java/com/siwei/web/SiWeiApplication.java
@@ -0,0 +1,31 @@
+package com.siwei.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * Author     ：chenzr.
+ * CreateDate ：2021/10/16 22:39
+ * Description：启动项
+ * Modified By：
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class SiWeiApplication {
+
+    public static void main(String[] args)
+    {
+        // System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(SiWeiApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+    }
+}
---
+++ siwei-system/src/main/java/com/siwei/system/controller/monitor/ServerController.java
@@ -0,0 +1,27 @@
+package com.siwei.system.controller.monitor;
+
+import com.siwei.common.core.domain.AjaxResult;
+import com.siwei.system.domain.web.Server;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 服务器监控
+ * 
+ * @author ruoyi
+ */
+@RestController
+@RequestMapping("/monitor/server")
+public class ServerController
+{
+    @PreAuthorize("@ss.hasPermi('monitor:server:list')")
+    @GetMapping()
+    public AjaxResult getInfo() throws Exception
+    {
+        Server server = new Server();
+        server.copyTo();
+        return AjaxResult.success(server);
+    }
+}
